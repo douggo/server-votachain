23780d20b6e6d7b139e03e6b25e6d16f3f136284
Douglas Felipe da Silva <douglas.silva@unidavi.edu.br>
2018-09-09 20:42:24 -0300

    Adição de WebSockets (ws) para gerenciar a conexão peer-to-peer da blockchain através do p2p-server.js

---
+++ app/p2p-server.js
@@ -0,0 +1,34 @@
+const Websocket = require('ws');
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+class P2pServer {
+    constructor(blockchain) {
+        this.blockchain = blockchain;
+        this.sockets = [];
+    }
+
+    escutar() {
+        const server = new Websocket.Server({ port: P2P_PORT });
+        server.on('connection', socket => this.conectaSocket(socket));
+
+        this.conectaPeers();
+
+        console.log(`Escutando por conexões peer-to-peer na porta: ${P2P_PORT}`);
+    }
+
+    conectaPeers() {
+        peers.forEach(peer => {
+            const socket = new Websocket(peer);
+            socket.on('open', () => this.conectaSocket(socket));
+        });
+    }
+
+    conectaSocket(socket) {
+        this.sockets.push(socket);
+        console.log('Socket conectado');
+    }
+}
+
+module.exports = P2pServer;
