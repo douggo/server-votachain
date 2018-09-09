f0b6eac606ab3db2123758e4f3e06993a790a83f
Douglas Felipe da Silva <douglas.silva@unidavi.edu.br>
2018-09-09 19:41:27 -0300

    Adição do body-parser para realizar a captação do JSON enviado para a API

--- app/index.js
+++ app/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = bc.adicionaBlock(req.body.dado);
+    console.log(`Novo bloco adicionado: ${block.toString()}`);
+    res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Ouvindo na porta ${HTTP_PORT}`));
