747a0733ce3a54d39daff9bc840ac489334eb8ee
Douglas Felipe da Silva <douglas.silva@unidavi.edu.br>
2018-10-02 09:33:50 -0300

    Setando no app.listen para não pegar nenhum host, ficando como '0.0.0.0'.

--- app/index.js
+++ app/index.js
@@ -22,5 +22,5 @@ app.post('/mine', (req, res) => {
     res.redirect('/blocks');
 });
 
-app.listen(HTTP_PORT, () => console.log(`Escutando na porta ${HTTP_PORT}`));
+app.listen(HTTP_PORT, '0.0.0.0', () => console.log(`Escutando na porta ${HTTP_PORT}`));
 p2pServer.escutar();
