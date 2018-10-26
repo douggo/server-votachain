530e803f96706e021c7bf01c24632f3e7c33dce7
Douglas Felipe da Silva <douglas.silva@unidavi.edu.br>
2018-10-26 01:04:55 -0300

    Remoção do Timestamp
    Remoção do timestamp da Blockchain, para não realizar a inclusão do dia/mês/ano hora:minuto:segundo em que o block foi inserido na blockchain.
    
    Questão de anonimidade de votação.

--- blockchain/block.js
+++ blockchain/block.js
@@ -2,8 +2,7 @@ const SHA256 = require('crypto-js/sha256');
 const { DIFICULDADE } = require('../config');
 
 class Block {
-    constructor(timestamp, hashAnterior, hash, id_eleicao, id_candidato, nonce, DIFICULDADE) {
-        this.timestamp = timestamp;
+    constructor(hashAnterior, hash, id_eleicao, id_candidato, nonce, DIFICULDADE) {
         this.hashAnterior = hashAnterior;
         this.hash = hash,
         this.id_eleicao = id_eleicao;
@@ -14,41 +13,41 @@ class Block {
 
     toString() {
         return `Block - 
-            Timestamp    : ${this.timestamp}
             Hash anterior: ${this.hashAnterior.substring(0, 10)}
             Hash         : ${this.hash.substring(0, 10)}
             id_eleicao   : ${this.id_eleicao}
             id_candidato : ${this.id_candidato}
             Nonce        : ${this.nonce}
-            Dificuldade  : ${this.dificuldade}`;
+            Dificuldade  : ${this.dificuldade}
+            
+            `;
     }
 
     static genesis() {
-        return new this('Inicío', '------', 'pr1m31r0-h45h', 0, 0, 0, DIFICULDADE);
+        return new this('------', 'pr1m31r0-h45h', 0, 0, 0, DIFICULDADE);
     }
 
     static minerarBlock(blockAnterior, id_eleicao, id_candidato) {
-        let hash, timestamp;
+        let hash;
         const hashAnterior = blockAnterior.hash;
         let dificuldade;
         let nonce = 0;
         do {
             nonce++;
-            timestamp = Date.now();
             dificuldade = DIFICULDADE;
-            hash = Block.hash(timestamp, hashAnterior, id_eleicao, id_candidato, nonce, dificuldade);
+            hash = Block.hash(hashAnterior, id_eleicao, id_candidato, nonce, dificuldade);
         } while (hash.substring(0, dificuldade) !== '0'.repeat(dificuldade));
 
-        return new this(timestamp, hashAnterior, hash, id_eleicao, id_candidato, nonce, dificuldade);
+        return new this(hashAnterior, hash, id_eleicao, id_candidato, nonce, dificuldade);
     }
 
-    static hash(timestamp, hashAnterior, id_eleicao, id_candidato, nonce, dificuldade) {
-        return SHA256(`${timestamp}${hashAnterior}${id_eleicao}${id_candidato}${nonce}${dificuldade}`).toString();
+    static hash(hashAnterior, id_eleicao, id_candidato, nonce, dificuldade) {
+        return SHA256(`${hashAnterior}${id_eleicao}${id_candidato}${nonce}${dificuldade}`).toString();
     }
 
     static blockHash(block) {
-        const { timestamp, hashAnterior, id_eleicao, id_candidato, nonce, dificuldade } = block;
-        return Block.hash(timestamp, hashAnterior, id_eleicao, id_candidato, nonce, dificuldade);
+        const { hashAnterior, id_eleicao, id_candidato, nonce, dificuldade } = block;
+        return Block.hash(hashAnterior, id_eleicao, id_candidato, nonce, dificuldade);
     }
 
 }
